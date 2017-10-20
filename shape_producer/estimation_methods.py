599e4a67f0292b87a8f427ac2e81be478c542845
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-20 09:05:40 +0200

    enable setter for name of estimation methods

--- shape_producer/estimation_methods.py
+++ shape_producer/estimation_methods.py
@@ -28,6 +28,10 @@ class EstimationMethod(object):
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, name):
+        self._name = name
+
     @property
     def era(self):
         return self._era

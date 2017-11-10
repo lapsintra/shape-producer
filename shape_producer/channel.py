d1ea3fffb203372417d95f5d124492f5d2e88987
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-10 11:02:33 +0100

    remoge trg_singlemuon und trg_singleelectron for crappy smhtt analysis

--- shape_producer/channel.py
+++ shape_producer/channel.py
@@ -24,7 +24,7 @@ class MT(Channel):
                 "againstElectronDiscriminator"),
             Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
             Cut("iso_1<0.15", "muon_iso"),
-            Cut("q_1*q_2<0", "os"), Cut("trg_singlemuon==1", "trg_singlemuon"))
+            Cut("q_1*q_2<0", "os"))
 
     @property
     def cuts(self):
@@ -47,7 +47,7 @@ class ET(Channel):
                 "againstElectronDiscriminator"),
             Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
             Cut("iso_1<0.1", "ele_iso"),
-            Cut("q_1*q_2<0", "os"), Cut("trg_singleelectron==1", "trg_singleelectron"))
+            Cut("q_1*q_2<0", "os"))
 
     @property
     def cuts(self):

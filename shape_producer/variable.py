e2954c27d5b3fe529fae6082e975deeeb25e586c
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-12 18:06:27 +0100

    add expression property to variables; makes it possible to unroll distributions

--- shape_producer/variable.py
+++ shape_producer/variable.py
@@ -9,9 +9,13 @@ logger = logging.getLogger(__name__)
 
 # Class to store a variable and its binning
 class Variable(object):
-    def __init__(self, name, binning):
+    def __init__(self, name, binning, expression=None):
         self._name = name
         self._binning = binning
+        if expression == None:
+            self._expression = name
+        else:
+            self._expression = expression
 
     @property
     def name(self):
@@ -20,3 +24,7 @@ class Variable(object):
     @property
     def binning(self):
         return self._binning
+
+    @property
+    def expression(self):
+        return self._expression

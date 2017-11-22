9eb4c16d3bb42c7142227b0a8ea3ecd755fe25a0
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2017-11-22 16:59:14 +0100

    yapf formatting

--- shape_producer/channel.py
+++ shape_producer/channel.py
@@ -25,7 +25,8 @@ class MT(Channel):
             Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
             Cut("iso_1<0.15", "muon_iso"),
             Cut("q_1*q_2<0", "os"),
-            Cut("((trg_singlemuon==1 && pt_1>23 && pt_2>30) + (trg_mutaucross==1 && pt_1>20 && pt_1<=23 && pt_2>30))", "trg_singlemuoncross"))
+            Cut("((trg_singlemuon==1 && pt_1>23 && pt_2>30) + (trg_mutaucross==1 && pt_1>20 && pt_1<=23 && pt_2>30))",
+                "trg_singlemuoncross"))
 
     @property
     def cuts(self):
@@ -49,7 +50,8 @@ class ET(Channel):
             Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
             Cut("iso_1<0.1", "ele_iso"),
             Cut("q_1*q_2<0", "os"),
-            Cut("(trg_singleelectron==1 && pt_1>26 && pt_2>30)", "trg_singleelectron"))
+            Cut("(trg_singleelectron==1 && pt_1>26 && pt_2>30)",
+                "trg_singleelectron"))
 
     @property
     def cuts(self):
